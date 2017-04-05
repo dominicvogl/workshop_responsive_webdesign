4ca0cb48ecd4bd150f3fb70a087dc74631d86384
Dominic <dv@cat-ia.de>
2017-04-06 00:33:50 +0200

    add macy for masonry scripting

---
+++ 04_Projekt/js/setup.js
@@ -0,0 +1,12 @@
+// Setup Macy Masonry Script
+
+var macy  = Macy({
+   container: '#image--feed',
+   trueOrder: false,
+   waitForImages: false,
+   margin: 1,
+   columns: 3,
+   breakAt: {
+      460: 2
+   }
+});
